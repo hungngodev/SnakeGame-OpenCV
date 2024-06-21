016dfd54fd2c36051e2b596396c747028891b2f9
hungngodev <hungngo1607@gmail.com>
2024-06-21 11:00:26 +0700

    if else trial

--- index.py
+++ index.py
@@ -39,7 +39,7 @@ def touchBody(snakeBody, snakeHead):
     # snakeHead = snakeBody[0]
     return snakeHead in snakeBody[1:]
 
-def drawing(snakeHead, img, snakeBody,apple, curDir, pathSolution):
+def drawing(snakeHead, img, snakeBody,apple, curDir, ):
     img = np.zeros((width *square, length*square,3), dtype='uint8')
     cv2.rectangle(img,(apple[0],apple[1]),(apple[0]+square,apple[1]+square),(0,0,255),-1)
     cv2.rectangle(img, (snakeHead[0], snakeHead[1]), (snakeHead[0]+square, snakeHead[1]+square), snakeHeadColor, 3)
@@ -54,12 +54,7 @@ def drawing(snakeHead, img, snakeBody,apple, curDir, pathSolution):
     for i in range(square//speed -1 , len(snakeBody), square//speed):
         cv2.circle(img, (snakeBody[i][0], snakeBody[i][1]), 5, eyeColor, -1)
         cv2.rectangle(img, (snakeBody[i][0], snakeBody[i][1]), (snakeBody[i][0]+square, snakeBody[i][1]+square), snakeBodyColor, 3)
-    
-    if autoPlay:
-        for i in pathSolution['path']:
-            cv2.rectangle(img, (i[0], i[1]), (i[0]+square, i[1]+square), (255,255,255), 1)
-        for i in pathSolution['closedList']:
-            cv2.rectangle(img, (i[0], i[1]), (i[0]+square, i[1]+square), (255,123,98), 1)
+        
     cv2.imshow('a',img)
     return img
 
@@ -121,56 +116,64 @@ def setUpGame():
 
 gameState= setUpGame()
 
-def setUpSolutionState(gameState):
-    if autoPlay == False: return {}
-    solution = a_star_search( np.ones((width *square, length*square), dtype='uint8'), gameState['snakeHead'], gameState['apple'], gameState['curDir'], gameState['snakeBody'], square, length*square, width*square, speed)
-    solutionState = {
-        "path": solution[0],
-        "closedList": solution[1],
-        "currentStep": 0,
-        "changingDirection": False,
-    }
-    return solutionState
 
-def playFunc(gameState, solutionState):
-    gameState['curDir'] = solutionState['path'][solutionState['currentStep']][2]
-    solutionState['currentStep'] += 1
+
+def playFunc(gameState):
+    applePerspective = [gameState['apple'][0] - gameState['snakeHead'][0], gameState['apple'][1] - gameState['snakeHead'][1]]
+    prevDir = gameState['prevDir']
     
-solutionState = setUpSolutionState(gameState)
+    if applePerspective[0] > 0 and prevDir != 0:
+        gameState['curDir'] = 1
+    elif applePerspective[0] < 0 and prevDir != 1:
+        gameState['curDir'] = 0
+    elif applePerspective[1] > 0 and prevDir != 3:
+        gameState['curDir'] = 2
+    elif applePerspective[1] < 0 and prevDir != 2:
+        gameState['curDir'] = 3
+    if applePerspective[0] > 0:
+        if prevDir !=0:
+            gameState['curDir'] = 1
+        else: 
+            if applePerspective[1] > 0:
+                gameState['curDir'] = 2
+            else:
+                gameState['curDir'] = 3
+    elif applePerspective[0] < 0:
+        if prevDir !=1:
+            gameState['curDir'] = 0
+        else: 
+            if applePerspective[1] > 0:
+                gameState['curDir'] = 2
+            else:
+                gameState['curDir'] = 3
+    elif applePerspective[1] > 0:
+        if prevDir !=3:
+            gameState['curDir'] = 2
+        else: 
+            if applePerspective[0] > 0:
+                gameState['curDir'] = 1
+            else:
+                gameState['curDir'] = 0
+    elif applePerspective[1] < 0:
+        if prevDir !=2:
+            gameState['curDir'] = 3
+        else: 
+            if applePerspective[0] > 0:
+                gameState['curDir'] = 1
+            else:
+                gameState['curDir'] = 0
+    
+    pass
+    
 
 while True:
-    gameState['img'] = drawing(gameState['snakeHead'], gameState['img'], gameState['snakeBody'], gameState['apple'], gameState['curDir'], solutionState)
+    gameState['img'] = drawing(gameState['snakeHead'], gameState['img'], gameState['snakeBody'], gameState['apple'], gameState['curDir'])
         
     k = takeKeyInput(gameState)
     if k == -2: break
     
     if autoPlay: 
-        try:
-            if solutionState['changingDirection']:
-                    solutionState= setUpSolutionState(gameState)
-                    solutionState['changingDirection'] = False
-            playFunc(gameState, solutionState)
-        except:
-            print("Not Reachable")
-            cv2.putText(gameState['img'],'Your Score is {}'.format(gameState['score']),(140,250), font, 1,(255,255,255),2,cv2.LINE_AA)
-            cv2.imshow('a',gameState['img'])
-            k = cv2.waitKey(0)
-            if k == ord('q'): break
-            if k == ord('r'):            
-                gameState= {
-                    "img": np.zeros((width *square, length*square,3), dtype='uint8'),
-                    "score": 0,
-                    "prevDir": 1,
-                    "key": 1,
-                    "save": False,
-                    "firstTime": False,
-                    "snakeHead": randomPoint(),
-                    "apple": randomPoint(),
-                    "curDir": np.random.randint(0,4),
-                }
-            gameState['snakeBody'] = createSnakeBody(gameState['snakeHead'], gameState['curDir'])
-            solutionState= setUpSolutionState(gameState)
-            continue
+        playFunc(gameState)
 
     gameState['snakeBody'].insert(0,list(gameState['snakeHead']))  
     gameState['prevDir'] = gameState['curDir']
@@ -187,8 +190,6 @@ while True:
     if gameState['snakeHead'] == gameState['apple']:
         gameState['apple'], gameState['score'] = eatApple(gameState['apple'], gameState['score'], gameState['snakeBody'], gameState['snakeHead'])
         gameState['snakeBody'].extend([list(gameState['snakeBody'][-1])]* (round(square//speed)-1))
-        if autoPlay:
-            solutionState['changingDirection'] = True
     else:
         gameState['snakeBody'].pop()
          
@@ -218,7 +219,7 @@ while True:
                 "curDir": np.random.randint(0,4),
             }
             gameState['snakeBody'] = createSnakeBody(gameState['snakeHead'], gameState['curDir'])
-            solutionState= setUpSolutionState(gameState)
+    
 
 
 
